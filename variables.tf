fe7b26418693d63be5cbdc3602689c1b76c11358
Alyson Rodrigo <alyson.rodrigo@cookiesoft.com.br>
2024-12-01 03:07:14 -0300

    Ajustes recursos infra desafio moutsti

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "pvt_key" {
-  description = "Caminho para a chave privada SSH"
-  sensitive   = true
-}
-
 variable "do_token" {
   description = "Token de autenticação da DigitalOcean"
   sensitive   = true
